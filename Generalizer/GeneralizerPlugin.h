554d7d8a45101a0af26232b97401c20aa3eb4d36
Ivan Stojic <ivan.stojic@gmail.com>
2012-01-02 19:36:25 +0100

    Initial Commit

---
+++ Generalizer/GeneralizerPlugin.h
@@ -0,0 +1,20 @@
+//
+//  GeneralizerPlugin.h
+//  Generalizer
+//
+//  Created by Ivan Stojic on 1/2/12.
+//  Copyright (c) 2012 __MyCompanyName__. All rights reserved.
+//
+
+#import <Quartz/Quartz.h>
+
+@interface GeneralizerPlugIn : QCPlugIn
+
+/*
+Declare here the properties to be used as input and output ports for the plug-in e.g.
+@property double inputFoo;
+@property (copy) NSString* outputBar;
+You can access their values in the appropriate plug-in methods using self.inputFoo or self.inputBar
+*/
+
+@end
